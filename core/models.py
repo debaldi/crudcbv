adb10fec97bb207e316e5bc8fa10212bce7efb46
Emerson <edbemerson@gmail.com>
2021-08-16 23:12:07 -0400

    Alterado em models max_digits

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.nome
--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from .views import IndexView, CreateProdutoView, UpdateProdutoView, DeleteProdutoView
 
+# Rota redirecionada de urls.py do projeto
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('add/', CreateProdutoView.as_view(), name='add_produto'),
     path('<int:pk>/update/', UpdateProdutoView.as_view(), name='upd_produto'),
     path('<int:pk>/delete/', DeleteProdutoView.as_view(), name='del_produto'),
 ]
-
