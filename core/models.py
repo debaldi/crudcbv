96ade7f0df80aeaa48daf909bd116201414922e1
Emerson <edbemerson@gmail.com>
2021-08-16 23:25:54 -0400

    Alterando a forma dos botoes de <buttons> para link <a>

--- core/models.py
+++ core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    nome = models.CharField('Produto', max_length=100)
+    preco = models.DecimalField('preço', max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.nome
